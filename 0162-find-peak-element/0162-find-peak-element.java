4aba64f7c7d35fe6f902e5c38e23d7fca5252d97
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2025-01-22 17:40:02 +0530

    Time: 0 ms (100%), Space: 42.9 MB (13.03%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -3,35 +3,45 @@ class Solution {
         int low = 0;
         int high = nums.length-1;
         
+        // while(low<high){
+        //     int mid =low+ (high-low)/2;
+
+        //     if(mid>0 && mid< high){
+        //         if(nums[mid]> nums[mid-1] && nums[mid]>nums[mid+1]){
+        //             return mid;
+        //         }
+        //         else if(nums[mid]<nums[mid-1]){
+        //             high = mid-1;
+        //         }
+        //         else{
+        //             low = mid+1;
+        //         }
+        //     }
+        //     else if(mid == 0 ){
+        //         if(nums[mid] > nums[mid+1]){
+        //             return mid;
+        //         }
+        //         else{
+        //             return mid+1;
+        //         }
+        //     }
+        //     else{
+        //         if(nums[mid] > nums[mid-1]){
+        //             return mid;
+        //         }
+        //         else{
+        //             return mid-1;
+        //         }
+        //     }
+        // }
         while(low<high){
             int mid =low+ (high-low)/2;
 
-            if(mid>0 && mid< high){
-                if(nums[mid]> nums[mid-1] && nums[mid]>nums[mid+1]){
-                    return mid;
-                }
-                else if(nums[mid]<nums[mid-1]){
-                    high = mid-1;
-                }
-                else{
-                    low = mid+1;
-                }
-            }
-            else if(mid == 0 ){
-                if(nums[mid] > nums[mid+1]){
-                    return mid;
-                }
-                else{
-                    return mid+1;
-                }
+            if(nums[mid] >= nums[mid+1]){
+                high = mid;
             }
             else{
-                if(nums[mid] > nums[mid-1]){
-                    return mid;
-                }
-                else{
-                    return mid-1;
-                }
+                low = mid+1;
             }
         }
         return low;
