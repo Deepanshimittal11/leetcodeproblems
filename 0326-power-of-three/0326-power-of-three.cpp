3bc1bca5005a921579e6ff17ea54f20ba6ee3538
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2025-01-27 18:32:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n<=0){
+            return false;
+        }
+        // for(int i=0;i<n;i++){
+        //     if(n==pow(3,i)){
+        //         return true;
+        //     }
+        // }
+        if(n%x==0){
+            return true;
+        }
+        return false;
+    }
+};
