50ee6e225b348313eac20304bca2bf251cfdb41d
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2025-02-17 18:45:16 +0530

    Time: 1575 ms (5.04%), Space: 47.5 MB (5.34%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,15 +1,35 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        map.put(0,1);
-        int preSum =0;
-        int cnt =0;
-        for(int i=0;i<nums.length;i++){
-            preSum += nums[i];
-            int remove = preSum-k ;
-            cnt += map.getOrDefault(remove, 0);
-            map.put(preSum, map.getOrDefault(preSum, 0) + 1); 
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // map.put(0,1);
+        // int preSum =0;
+        // int cnt =0;
+        // for(int i=0;i<nums.length;i++){
+        //     preSum += nums[i];
+        //     int remove = preSum-k ;
+        //     cnt += map.getOrDefault(remove, 0);
+        //     map.put(preSum, map.getOrDefault(preSum, 0) + 1); 
+        // }
+        // return cnt;
+
+        return generateSubsets(nums, 0,0, k);
+        
+    }
+    private int generateSubsets(int[] nums, int start, int s, int k){
+        if(start==nums.length){
+            return 0;
         }
+        int cnt =0;
+        int sum =s;
+        for(int i=start;i<nums.length;i++){
+            sum+=nums[i];
+
+            if(sum==k){
+                cnt++;
+            }
+        }
+
+        cnt += generateSubsets(nums,start+1, s, k);
         return cnt;
     }
 }
