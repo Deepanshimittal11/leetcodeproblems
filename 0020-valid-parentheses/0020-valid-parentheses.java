b88a454efe6a0211919b46c299c4491b3198ede3
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2025-03-08 21:55:25 +0530

    Time: 2 ms (97.74%), Space: 41.7 MB (65.33%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,16 +1,24 @@
 class Solution {
     public boolean isValid(String s) {
-        for(int i=0;i<s.length()-1;i++){
-            if(s.charAt(i)=='('&& s.charAt(i+1)==')'){
-                return true;
+        Stack<Character> stack = new Stack<>();
+
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+
+            if(ch== '('|| ch== '{'|| ch=='['){
+                stack.add(ch);
             }
-            else if(s.charAt(i)=='['&& s.charAt(i+1)==']'){
-                return true;
-            }
-            else if(s.charAt(i)=='{'&& s.charAt(i+1)=='}'){
-                return true;
+
+            else if(ch==')' || ch=='}' || ch==']'){
+                if(stack.isEmpty() || !isMatchingPair(stack.pop(), ch)){
+                    return false;
+                }
             }
         }
-        return false;
+        return stack.isEmpty();
+    }
+
+    private boolean isMatchingPair(char open, char close){
+        return(open=='('&& close==')' || open=='{'&& close=='}' || open=='['&& close==']');
     }
 }
