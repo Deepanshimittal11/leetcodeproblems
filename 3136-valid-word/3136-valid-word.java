0fc14b2fb26e5a6aa38b789e430fcc6b12cb5884
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2025-07-15 18:12:39 +0530

    Time: 1 ms (99.12%), Space: 42.2 MB (47.02%) - LeetHub

--- 3136-valid-word/3136-valid-word.java
+++ 3136-valid-word/3136-valid-word.java
@@ -1,24 +1,21 @@
 class Solution {
     public boolean isValid(String word) {
-        if(word==null || word.length()<3) return false;
-
-        boolean hasVowel = false;
-        boolean hasConsonent = false;
-
+        if(word.length()<3) return false;
+        boolean has_vowel = false;
+        boolean has_consonent = false;
         for(char c : word.toCharArray()){
-            if(!Character.isLetterOrDigit(c)){
+            if(Character.isLetter(c)){
+                char ch = Character.toLowerCase(c);
+                if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+                    has_vowel = true;
+                }else{
+                    has_consonent = true;
+                }
+            }
+            else if(!Character.isDigit(c)){
                 return false;
             }
-            if(Character.isLetter(c)){
-                char lowerC = Character.toLowerCase(c);
-                if("aeiou".indexOf(lowerC) >= 0){
-                    hasVowel = true;
-                }
-                else{
-                    hasConsonent = true;
-                }
-            }
         }
-        return hasVowel && hasConsonent;
+        return has_vowel && has_consonent;
     }
 }
