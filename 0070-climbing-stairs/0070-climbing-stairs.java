3ba681312d3d789b134f5da40cdbbadf6a7a7dc1
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2025-09-18 14:11:22 +0530

    Time: 0 ms (100%), Space: 40.3 MB (83.07%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,25 @@
 class Solution {
     public int climbStairs(int n) {
-        int prev1 = 1;
-        int prev2 = 1;
-        int curr = 0;
-        for(int i=2;i<=n;i++){
-            curr = prev1+prev2;
-            prev1 = prev2;
-            prev2 = curr;
+        // this solution is done through recursion b/c the contraints are low.
+        // int prev1 = 1;
+        // int prev2 = 1;
+        // int curr = 0;
+        // for(int i=2;i<=n;i++){
+        //     curr = prev1+prev2;
+        //     prev1 = prev2;
+        //     prev2 = curr;
+        // }
+        // return prev2;
+
+        //we can do this with DP:
+        if(n==1) return 1;
+        if(n==2) return 2;
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i=3;i<=n;i++){
+            dp[i] = dp[i-1]+dp[i-2];
         }
-        return prev2;
+        return dp[n];
     }
 }
