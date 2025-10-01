e31a198c1329589aa65dc59a22467907fdb57a2e
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2025-10-01 10:57:34 +0530

    Time: 135 ms (8.77%), Space: 73.1 MB (47.95%) - LeetHub

--- 3523-make-array-non-decreasing/3523-make-array-non-decreasing.java
+++ 3523-make-array-non-decreasing/3523-make-array-non-decreasing.java
@@ -4,7 +4,7 @@ class Solution {
         Stack<Integer> st = new Stack<>();
 
         for(int i=n-1;i>=0;i--){
-            if(!st.isEmpty() && nums[i]>st.peek()){
+            while(!st.isEmpty() && nums[i]>st.peek()){
                 st.pop();
             }
             st.push(nums[i]);
