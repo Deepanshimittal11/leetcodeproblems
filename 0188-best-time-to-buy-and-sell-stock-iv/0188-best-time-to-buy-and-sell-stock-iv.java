9ed7b45d6e4bc0b2f8a15ff3204e5373d052f480
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2025-10-06 11:21:45 +0530

    Time: 9 ms (20.36%), Space: 44.7 MB (40.97%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -0,0 +1,46 @@
+class Solution {
+    //recursion:
+    // public int maxProfit(int k, int[] prices) {
+    //     return helper(prices, 0, 1, k);
+    // }
+    // public int helper(int[] arr, int i, int canbuy, int k){
+    //     if(i == arr.length) return 0;
+    //     if(k == 0) return 0;
+    //     if(canbuy == 1){
+    //         int buy = -arr[i] + helper(arr, i+1, 0, k);
+    //         int skip = helper(arr, i+1, 1, k);
+    //         return Math.max(buy, skip);
+    //     }else{
+    //         int sell = arr[i] + helper(arr, i+1, 1, k-1);
+    //         int hold = helper(arr, i+1, 0, k);
+    //         return Math.max(sell, hold);
+    //     }
+    // }
+
+    //dp:
+    public int maxProfit(int k, int[] prices) {
+        int n = prices.length;
+        int[][][] dp = new int[n][2][k+1];
+        for(int[][] row : dp){
+            for(int[] row1 : row){
+                Arrays.fill(row1,-1);
+            }
+        }
+        return helper(prices, 0, 1, k, dp);
+    }
+    public int helper(int[] arr, int i, int canbuy, int k, int[][][] dp){
+        if(i == arr.length) return 0;
+        if(k == 0) return 0;
+        if(dp[i][canbuy][k] != -1) return dp[i][canbuy][k];
+        if(canbuy == 1){
+            int buy = -arr[i] + helper(arr, i+1, 0, k, dp);
+            int skip = helper(arr, i+1, 1, k,dp);
+            dp[i][canbuy][k] = Math.max(buy, skip);
+        }else{
+            int sell = arr[i] + helper(arr, i+1, 1, k-1, dp);
+            int hold = helper(arr, i+1, 0, k, dp);
+            dp[i][canbuy][k] = Math.max(sell, hold);
+        }
+        return dp[i][canbuy][k];
+    }
+}
