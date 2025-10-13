92e1cd28447d520c03e85388867fc2882459afcb
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2025-10-13 13:16:59 +0530

    Time: 111 ms (0.67%), Space: 41.2 MB (36.47%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,33 @@
+class Solution {
+    //recursion:
+    // public int rob(int[] nums) {
+    //     int n = nums.length;
+    //     return helper(nums,n-1);
+    // }
+    // public int helper(int[] nums,int i){
+    //     if(i<0) return 0;
+    //     if(i==0) return nums[0];
+
+    //     int rob = nums[i] + helper(nums, i-2);
+    //     int skip = helper(nums, i-1);
+    //     return Math.max(rob,skip);
+    // }
+
+    //dp:
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+        return helper(nums,n-1, dp);
+    }
+    public int helper(int[] nums,int i, int[] dp){
+        if(i<0) return 0;
+        if(i==0) return nums[0];
+        if(dp[i] != -1) return dp[i];
+
+        int rob = nums[i] + helper(nums, i-2, dp);
+        int skip = helper(nums, i-1, dp);
+        dp[i] = Math.max(rob,skip);
+        return dp[i];
+    }
+}
