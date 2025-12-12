99ec42c47a98bc70a80441c89e2bd5c8e60c60ad
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2025-12-12 12:42:45 +0530

    Time: 2 ms (92.64%), Space: 45.5 MB (59.91%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,23 +1,23 @@
 class Solution {
+    List<List<Integer>> ans;
+    
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        generateSubsets(0, candidates, target, ans, new ArrayList<>());
-        return ans;
+        ans = new ArrayList<>();
+        helper(candidates, target, 0, new ArrayList<>());
+        return ans;    
     }
-    private void generateSubsets(int ind, int[] arr, int target, List<List<Integer>> ans, List<Integer> ds){
-        if(target==0)
-            {
-                ans.add(new ArrayList<>(ds));
-                return;
-            }
-        if(ind == arr.length){
+
+    public void helper(int[] candidates, int target, int ind, List<Integer> ds){
+        if(ind == candidates.length) return;
+        if(target == 0){
+            ans.add(new ArrayList<>(ds));
             return;
         }
-        if(arr[ind]<=target){
-            ds.add(arr[ind]);
-            generateSubsets(ind, arr, target-arr[ind],ans,ds);
+        if(candidates[ind] <= target){
+            ds.add(candidates[ind]);
+            helper(candidates, target-candidates[ind], ind, ds);
             ds.remove(ds.size()-1);
         }
-        generateSubsets(ind+1,arr, target,ans,ds);
+        helper(candidates, target, ind+1, ds);
     }
 }
