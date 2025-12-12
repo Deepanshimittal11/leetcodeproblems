34938a0c5d62a4c7543deb543a8b67b35a69f1aa
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2025-12-12 13:03:05 +0530

    Time: 5 ms (69.53%), Space: 44.9 MB (60.38%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,26 +1,24 @@
 class Solution {
+    List<List<Integer>> ans;
+
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(candidates);
-        generateSubsets(candidates, 0, new ArrayList<>(), result, target, 0);
-        return result;
+        ans = new ArrayList<>();;
+        helper(candidates, target, 0, new ArrayList<>());
+        return ans;    
     }
 
-    private void generateSubsets(int[] candidates, int ind, List<Integer> current, List<List<Integer>> result, int target, int s){
-        if(s == target){
-            result.add(new ArrayList<>(current));
-            return ;
-        }
-        if(s>target || ind==candidates.length){
+    public void helper(int[] candidates, int target, int ind, List<Integer> ds){
+        if(target == 0){
+            ans.add(new ArrayList<>(ds));
             return;
         }
-        for(int i=ind;i<candidates.length;i++){
-            if(i>ind && candidates[i]==candidates[i-1]){
-                continue;
-            }
-            current.add(candidates[i]);
-        generateSubsets(candidates, i + 1, current, result, target, s+ candidates[i]); 
-        current.remove(current.size()-1);
+        for(int i=ind; i<candidates.length;i++){
+            if(i>ind && candidates[i]==candidates[i-1]) continue;
+            if(candidates[i] > target) break;
+            ds.add(candidates[i]);
+            helper(candidates, target-candidates[i], i+1, ds);
+            ds.remove(ds.size()-1);
         }
     }
 }
