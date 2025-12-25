949993198c2179779cf1799a76f36ea477ee6850
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2025-12-25 13:11:27 +0530

    Time: 43 ms (43.55%), Space: 109.5 MB (18.28%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
@@ -1,16 +1,13 @@
-import java.util.Arrays;
-
 class Solution {
     public long maximumHappinessSum(int[] happiness, int k) {
         int n = happiness.length;
-        long sum =0;
-
         Arrays.sort(happiness);
+        long sum = 0;
 
         for(int i=0;i<k;i++){
-            int index = n-1-i;
-            if(happiness[i] -i <= 0) break;
-            sum += happiness[index]-i;
+            int ind = n - i - 1;
+            if(happiness[ind]-i<=0) break;
+            sum += happiness[ind] -i;
         }
         return sum;
     }
