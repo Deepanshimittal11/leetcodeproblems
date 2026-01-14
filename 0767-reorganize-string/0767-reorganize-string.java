41a89410782d15b56bc3f9216783bb8792f4deec
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2026-01-14 19:24:16 +0530

    Time: 5 ms (39.31%), Space: 43.2 MB (44.2%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -1,6 +1,6 @@
 class pair{
     char ch;
-    int freq ;
+    int freq;
     pair(char ch, int freq){
         this.ch = ch;
         this.freq = freq;
@@ -9,35 +9,30 @@ class pair{
 class Solution {
     public String reorganizeString(String s) {
         Map<Character, Integer> mpp = new HashMap<>();
-        // first thing we'll do is to put each character in the map.
+        PriorityQueue<pair> pq = new PriorityQueue<>((a,b) -> b.freq-a.freq);
+
         for(char c : s.toCharArray()){
             mpp.put(c, mpp.getOrDefault(c,0)+1);
         }
-        PriorityQueue<pair> pq = new PriorityQueue<>((a,b) -> b.freq-a.freq);
-        //in this we had transfer map(key,value) to the heap
-        //in the form of pair.
-        for(Map.Entry<Character, Integer> m : mpp.entrySet()){
-            pq.offer(new pair(m.getKey(),m.getValue()));
+        for(char key : mpp.keySet()){
+            pq.offer(new pair(key, mpp.get(key)));
         }
-        //for the final result we'll make stringbuilder.
+
         StringBuilder sb = new StringBuilder();
         while(pq.size()>1){
-
-            pair firstChar = pq.poll();
-            sb.append(firstChar.ch);
-            firstChar.freq--;
-
-            pair secChar = pq.poll();
-            sb.append(secChar.ch);
-            secChar.freq--;
-
-            if(firstChar.freq>0) pq.offer(firstChar);
-            if(secChar.freq>0) pq.offer(secChar);
+            pair first = pq.poll();
+            pair sec = pq.poll();
+            sb.append(first.ch);
+            first.freq--;
+            sb.append(sec.ch);
+            sec.freq--;
+            if(first.freq > 0) pq.offer(first);
+            if(sec.freq > 0) pq.offer(sec);
         }
         if(!pq.isEmpty()){
-            pair last = pq.poll();
-            if(last.freq > 1) return "";
-            sb.append(last.ch);
+            pair node = pq.poll();
+            if(node.freq > 1) return "";
+            sb.append(node.ch);
         }
         return sb.toString();
     }
