f88e6d20a0a15099f32514c2596854cbb33124a1
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2026-01-17 00:03:02 +0530

    Time: 528 ms (64.29%), Space: 166.7 MB (57.14%) - LeetHub

---
+++ 2975-maximum-square-area-by-removing-fences-from-a-field/2975-maximum-square-area-by-removing-fences-from-a-field.java
@@ -0,0 +1,45 @@
+class Solution {
+    // long inf = 1000000007;
+    public int maximizeSquareArea(int m, int n, int[] hFences, int[] vFences) {
+        int inf = (int)1e9+7;
+
+        int[] h = new int[hFences.length+2];
+        int[] v = new int[vFences.length+2];
+
+        h[0] = 1;
+        h[h.length-1] = m;
+        for(int i=0;i<hFences.length;i++){
+            h[i+1] = hFences[i];
+        }
+
+        v[0] = 1;
+        v[v.length-1] = n;
+        for(int i=0;i<vFences.length;i++){
+            v[i+1] = vFences[i];
+        }
+
+        Arrays.sort(h);
+        Arrays.sort(v);
+
+        Set<Integer> set = new HashSet<>();
+
+        for(int i=0;i<h.length;i++){
+            for(int j=i+1;j<h.length;j++){
+                set.add(h[j]-h[i]);
+            }
+        }
+
+        long maxSide = 0;
+        for(int i=0;i<v.length;i++){
+            for(int j=i+1;j<v.length;j++){
+                int diff = v[j]-v[i];
+                if(set.contains(diff)){
+                    maxSide = Math.max(maxSide, diff);
+                }
+            }
+        }
+
+        if(maxSide == 0) return -1;
+        return (int)(maxSide*maxSide % inf);
+    }
+}
