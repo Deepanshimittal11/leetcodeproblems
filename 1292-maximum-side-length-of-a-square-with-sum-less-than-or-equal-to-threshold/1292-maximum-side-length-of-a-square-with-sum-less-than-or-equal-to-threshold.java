a25fe879ced0db970f167c2e07ddf658c9e88643
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2026-01-20 00:18:21 +0530

    Time: 20 ms (14.21%), Space: 57.9 MB (27.92%) - LeetHub

--- 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java
+++ 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java
@@ -3,14 +3,24 @@ class Solution {
         int n = mat.length;
         int m = mat[0].length;
 
+        int[][] prefix = new int[n+1][m+1];
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                prefix[i][j] =  prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1] + mat[i-1][j-1];
+            }
+        }
+
         int bestSideSq = 0;
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 for(int offset=0;offset<Math.min(n-i,m-j);offset++){
-                    int r2 = i + offset;
-                    int c2 = j + offset;
-                    int val = findsum(i,j,r2,c2, mat);
+                    int r2 = i + offset + 1;
+                    int c2 = j + offset + 1;
+
+                    // int val = findsum(i,j,r2,c2, mat);
+                    int val = prefix[r2][c2] - prefix[i][c2] - prefix[r2][j] + prefix[i][j];
+
                     if(val <= threshold){
                         bestSideSq = Math.max(bestSideSq, offset + 1);
                     }
@@ -22,13 +32,13 @@ class Solution {
         }
         return bestSideSq;
     }
-    public int findsum(int i, int j, int r2, int c2, int[][] mat){
-        int sum = 0;
-        for(int row=i;row<=r2;row++){
-            for(int col=j;col<=c2;col++){
-                sum += mat[row][col];
-            }
-        }
-        return sum;
-    }
+    // public int findsum(int i, int j, int r2, int c2, int[][] mat){
+    //     int sum = 0;
+    //     for(int row=i;row<=r2;row++){
+    //         for(int col=j;col<=c2;col++){
+    //             sum += mat[row][col];
+    //         }
+    //     }
+    //     return sum;
+    // }
 }
