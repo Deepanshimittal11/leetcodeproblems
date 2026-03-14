723d58b51ca7169bd26a35641e6f15587d400f75
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2026-03-14 17:17:39 +0530

    Time: 20 ms (15.44%), Space: 48.1 MB (80.9%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,16 +1,38 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int n = nums.length;
-        int[] ans = new int[n];
+        int[] list = new int[2*n];
         Stack<Integer> st = new Stack<>();
 
-        for(int i=2*n-1;i>=0;i--){
-            while(!st.isEmpty() && st.peek()<=nums[i%n]){
+        for(int i=0;i<n;i++){
+            list[i] = nums[i];
+            list[i+n] = nums[i];
+        }
+
+        //nums = [1,2,1];
+        //list = [1,2,1,1,2,1];
+
+        List<Integer> temp = new ArrayList<>();
+        int m = list.length;
+
+        for(int i=m-1;i>=0;i--){
+            while(!st.isEmpty() && st.peek()<=list[i]){
                 st.pop();
             }
-            if(st.isEmpty()) ans[i%n] = -1;
-            else ans[i%n] = st.peek();
-            st.push(nums[i%n]);
+
+            if(st.isEmpty()){
+                temp.add(-1);
+            }
+            else{
+                temp.add(st.peek());
+            }
+            st.push(list[i]);
+        }
+
+        int[] ans = new int[n];
+        Collections.reverse(temp);
+        for(int i=0;i<n;i++){
+            ans[i] = temp.get(i);
         }
         return ans;
     }
