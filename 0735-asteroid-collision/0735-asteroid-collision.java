61f5e04178c389c1a929e80ca429f1d4ac399f55
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2026-03-16 00:12:17 +0530

    Time: 5 ms (66.49%), Space: 47.3 MB (17.6%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,26 +1,33 @@
 class Solution {
-    public int[] asteroidCollision(int[] asteroids) {
+    public int[] asteroidCollision(int[] arr) {
+        int n = arr.length;
         Stack<Integer> st = new Stack<>();
-        for(int a : asteroids){
-            collision : {
-                while(!st.isEmpty() && st.peek()>0 && a<0){
-                    if(st.peek()< -a){
-                        st.pop();
-                        continue;
-                    }
-                    else if(st.peek() == -a){
-                        st.pop();
-                    }
-                    break collision;
+
+        for(int i=0;i<n;i++){
+            if(arr[i]>0) st.push(arr[i]);
+            else{
+                boolean destroy = false;
+                while(!st.isEmpty() && st.peek()>0 && Math.abs(arr[i]) > st.peek()){
+                    st.pop();
                 }
-                st.push(a);
+                if(!st.isEmpty() && st.peek()==Math.abs(arr[i])){
+                    st.pop();
+                    destroy = true;
+                }
+                else if(!st.isEmpty() && st.peek()>Math.abs(arr[i])){
+                    destroy = true;
+                }
+                if(!destroy){
+                    st.push(arr[i]);
+                }
             }
         }
 
-        int[] res = new int[st.size()];
-        for(int i=res.length-1;i>=0;i--){
-            res[i] = st.pop();
+        int[] ans = new int[st.size()];
+        int i=st.size()-1;
+        while(!st.isEmpty()){
+            ans[i--] = st.pop();
         }
-        return res;
+        return ans;
     }
 }
