137780f5d006bae2b8e1fbdbea2381c0e25393df
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2026-03-16 23:24:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -1,17 +1,38 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
         int n = tickets.length;
-        int total = 0;
-        int target = tickets[k];
 
+        //if we do it normal formula:
+
+        // int total = 0;
+        // int target = tickets[k];
+        // for(int i=0;i<n;i++){
+        //     if(i<=k){
+        //         total += Math.min(tickets[i], target);
+        //     }
+        //     else{
+        //         total += Math.min(tickets[i], target-1);
+        //     }
+        // }
+        // return total;
+
+
+        //with queue ds:
+        int time = 0;
+        Queue<int[]> q = new LinkedList<>();
         for(int i=0;i<n;i++){
-            if(i<=k){
-                total += Math.min(tickets[i], target);
-            }
-            else{
-                total += Math.min(tickets[i], target-1);
+            q.offer(new int[]{tickets[i],i});
+        }
+        while(true){
+            int[] person = q.poll();
+            time++;
+            person[0]--;
+
+            if(person[0]==0){
+                if(person[1]==k) return time;
+            }else{
+                q.offer(person);
             }
         }
-        return total;
     }
 }
