cadfcaeaef09628515454bbefec8a55228637cfe
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2026-03-17 17:10:38 +0530

    Time: 0 ms (100%), Space: 48 MB (84.03%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,49 +1,47 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] result = new int[]{-1, -1};
-        
-        result[0] = findLeft(nums, target);
-        result[1] = findRight(nums, target);
-        
-        return result;
+        int left = leftmost(nums, target);
+        int right = rightmost(nums, target);
+        return new int[]{left, right};
     }
-     private int findLeft(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-        int leftIndex = -1;
-        
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
-                leftIndex = mid;
-                right = mid - 1;  // Continue searching in the left half
-            } else if (nums[mid] < target) {
-                left = mid + 1;  // Search in the right half
-            } else {
-                right = mid - 1;  // Search in the left half
-            }
-        }
-        
-        return leftIndex;
-    }
-    
-    // Helper function to find the rightmost index of the target
-    private int findRight(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-        int rightIndex = -1;
-        
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
-                rightIndex = mid;
-                left = mid + 1;  // Continue searching in the right half
-            } else if (nums[mid] < target) {
-                left = mid + 1;  // Search in the right half
-            } else {
-                right = mid - 1;  // Search in the left half
-            }
-        }
-        
-        return rightIndex;
-    }
+    public int leftmost(int[] nums, int target){
+        int l = 0;
+        int r = nums.length-1;
+        int ans = -1;
 
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            if(nums[mid]==target){
+                ans = mid;
+                r = mid-1;  //move pointer left most to the target
+            }
+            else if(nums[mid]<target){
+                l = mid+1;
+            }
+            else{
+                r = mid-1;
+            }
+        }
+        return ans;
+    }
+    public int rightmost(int[] nums, int target){
+        int l = 0;
+        int r = nums.length-1;
+        int ans = -1;
+        
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            if(nums[mid]==target){
+                ans = mid;
+                l = mid+1; //move pointer right most to the target
+            }
+            else if(nums[mid]<target){
+                l = mid+1;
+            }
+            else{
+                r = mid-1;
+            }
+        }
+        return ans;
+    }
 }
