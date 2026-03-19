0ed816d99a24ca04a1c0b4a3cff881eca9458453
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2026-03-20 00:17:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,14 +3,23 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
         vector<int> ans;
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         if(nums[i]+nums[j]==target){
+        //             ans.push_back(i);
+        //             ans.push_back(j);
+        //         }
+        //     }
+        // }
+
+        unordered_map<int, int> mpp;
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]+nums[j]==target){
-                    ans.push_back(i);
-                    ans.push_back(j);
-                }
+            int temp = target-nums[i];
+            if(mpp.find(temp) != mpp.end()){
+                return {mpp[temp], i};
             }
+            mpp[nums[i]] = i;
         }
-        return ans;
+        return {};
     }
 };
