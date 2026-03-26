1dae57492ef4ea30fec193bf8ec7e0f99b053e74
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2026-03-26 11:25:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,15 +1,18 @@
 class Solution {
+    boolean[] dp;
     public boolean canJump(int[] nums) {
-        int maxReach = 0;
-        for(int i=0;i<nums.length;i++){
-            if(i>maxReach){
-                return false;
-            }
-            maxReach = Math.max(maxReach , i+nums[i]);
-            if(maxReach >= nums.length-1){
-                return true;
-            }
+        int n = nums.length;
+        dp = new boolean[n];
+        return solve(nums, n, 0);
+    }
+    public boolean solve(int[] nums, int n, int ind){
+        if(ind==n-1) return true;
+        if(ind>=n) return false;
+        if(dp[ind] != false) return dp[ind];
+
+        for(int i=1;i<=nums[ind];i++){
+            if(solve(nums, n, ind+i)==true) return dp[ind] = true;
         }
-        return false;
+        return dp[ind] = false;
     }
 }
