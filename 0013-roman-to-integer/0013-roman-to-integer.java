5af2e87cab98ba42c76ee7c9d1efbb594a6936e3
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2026-04-16 20:15:08 +0530

    Time: 4 ms (59.86%), Space: 47.2 MB (7.57%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,25 +1,40 @@
 class Solution {
     public int romanToInt(String s) {
-        Map<Character, Integer> romanMap = new HashMap<>();
-        romanMap.put('I', 1);
-        romanMap.put('V', 5);
-        romanMap.put('X', 10);
-        romanMap.put('L', 50);
-        romanMap.put('C', 100);
-        romanMap.put('D', 500);
-        romanMap.put('M', 1000);
-        
-        int result = 0;
-        for (int i = 0; i < s.length(); i++) {
-            int currentVal = romanMap.get(s.charAt(i));
-            
-            if (i + 1 < s.length() && currentVal < romanMap.get(s.charAt(i + 1))) {
-                result -= currentVal;  
-            } else {
-                result += currentVal;  
+        HashMap<Character, Integer> mpp = new HashMap<>();
+        mpp.put('I', 1);
+        mpp.put('V', 5);
+        mpp.put('X', 10);
+        mpp.put('L', 50);
+        mpp.put('C', 100);
+        mpp.put('D', 500);
+        mpp.put('M', 1000);
+
+        //I can be placed before V (5) and X (10) to make 4 and 9. 
+        // X can be placed before L (50) and C (100) to make 40 and 90. 
+        // C can be placed before D (500) and M (1000) to make 400 and 900.
+        int n = s.length();
+        int ans = 0, i=0;
+        while(i<n-1){
+            char ch = s.charAt(i);
+            char ch1 = s.charAt(i+1);
+            if(ch=='I' && (ch1=='V' || ch1=='X')){
+                ans += (mpp.get(ch1) - mpp.get(ch));
+                i+=2;
+            }
+            else if(ch=='X' && (ch1=='L' || ch1=='C')){
+                ans += (mpp.get(ch1) - mpp.get(ch));
+                i+=2;
+            }
+            else if(ch=='C' && (ch1=='D' || ch1=='M')){
+                ans += (mpp.get(ch1) - mpp.get(ch));
+                i+=2;
+            }
+            else{
+                ans += mpp.get(ch);
+                i++;
             }
         }
-        
-        return result;
+        if(i==n-1) ans = ans+mpp.get(s.charAt(n-1));
+        return ans;
     }
 }
