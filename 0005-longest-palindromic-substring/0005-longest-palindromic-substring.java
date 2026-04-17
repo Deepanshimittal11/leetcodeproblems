17079ad16da68f752ebc09bc022d58b38bc731c8
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2026-04-18 00:49:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,9 +1,14 @@
 class Solution {
+    int[][] dp;
     public String longestPalindrome(String s) {
+        dp = new int[1001][1001];
+        for(int[] row : dp){
+            Arrays.fill(row, -1);
+        }
         int maxlen = 0, sp = 0;
         for(int i=0;i<s.length();i++){
             for(int j=i;j<s.length();j++){
-                if(isPalin(i, j, s)){
+                if(isPalin(i, j, s)==1){
                     if(j-i+1 > maxlen){
                         maxlen = j-i+1;
                         sp = i;
@@ -13,9 +18,10 @@ class Solution {
         }
         return s.substring(sp, sp+maxlen);
     }
-    public boolean isPalin(int i, int j, String s){
-        if(i>=j) return true;
-        if(s.charAt(i)==s.charAt(j)) return isPalin(i+1, j-1, s);
-        return false;
+    public int isPalin(int i, int j, String s){
+        if(i>=j) return 1;
+        if(dp[i][j] != -1) return dp[i][j];
+        if(s.charAt(i)==s.charAt(j)) return dp[i][j] = isPalin(i+1, j-1, s);
+        return dp[i][j] = 0;
     }
 }
