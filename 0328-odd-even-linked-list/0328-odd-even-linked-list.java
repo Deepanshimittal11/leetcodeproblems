e727acba0b9df60b991305df045ebd50050f7115
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2026-04-20 17:17:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -12,29 +12,45 @@ class Solution {
     public ListNode oddEvenList(ListNode head) {
         if(head==null || head.next==null) return head;
 
-        int cnt = 0;
-        ListNode odd = new ListNode(-1);
-        ListNode odd1 = odd;
+        // int cnt = 0;
+        // ListNode odd = new ListNode(-1);
+        // ListNode odd1 = odd;
 
-        ListNode even = new ListNode(-1);
-        ListNode even1 = even;
+        // ListNode even = new ListNode(-1);
+        // ListNode even1 = even;
 
-        ListNode temp = head;
-        while(temp!=null){
-            if(cnt % 2 != 0){
-                even1.next = temp;
-                even1 = even1.next;
-            }
-            else{
-                odd1.next = temp;
-                odd1 = odd1.next;
-            }
-            temp = temp.next;
-            cnt++;
+        // ListNode temp = head;
+        // while(temp!=null){
+        //     if(cnt % 2 != 0){
+        //         even1.next = temp;
+        //         even1 = even1.next;
+        //     }
+        //     else{
+        //         odd1.next = temp;
+        //         odd1 = odd1.next;
+        //     }
+        //     temp = temp.next;
+        //     cnt++;
+        // }
+        // even1.next = null;
+        // odd1.next = even.next;
+        // return odd.next;
+
+
+        // optimized approch:
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenhead = even;
+
+        while(even!=null && even.next!=null){
+            odd.next = even.next;
+            odd = odd.next;
+
+            even.next = odd.next;
+            even = even.next;
         }
 
-        even1.next = null;
-        odd1.next = even.next;
-        return odd.next;
+        odd.next = evenhead;
+        return head;
     }
 }
