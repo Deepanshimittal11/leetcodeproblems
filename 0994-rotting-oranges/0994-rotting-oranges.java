bf2e5674652e3955c441c6ba4548b1da67c142ca
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2026-05-26 10:34:32 +0530

    Time: 2 ms (90.51%), Space: 44.1 MB (55.78%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,41 +1,43 @@
 class Node{
-    int row, col, time;
-    Node(int i, int j, int t){
-        row = i;
-        col = j;
-        time = t;
+    int row;
+    int col;
+    int time;
+    Node(int row, int col, int time){
+        this.row = row;
+        this.col = col;
+        this.time = time;
     }
 }
 class Solution {
-    public boolean isValid(int nr, int nc, int rows, int cols){
-        return (nr>=0 && nc>=0 && nr<rows && nc<cols);
-    }
     public int orangesRotting(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
 
-        Queue<Node> q = new ArrayDeque<>();
         int fresh = 0;
+        Queue<Node> q = new LinkedList<>();
+
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
-                    q.offer(new Node(i,j,0));
+                    q.offer(new Node(i, j, 0));
                 }
-                else if(grid[i][j]==1){
+                else if(grid[i][j] == 1){
                     fresh++;
                 }
             }
         }
-        if(fresh==0) return 0;
+
+        if(fresh == 0) return 0;
 
         int mintime = 0;
-        int[][] dir = {{-1,0},{1,0},{0,1},{0,-1}};
+        int[][] dir = {{-1,0},{1,0},{0,-1},{0,1}};
+
         while(!q.isEmpty()){
             Node curr = q.poll();
-            for(int[] d : dir){
-                int nr = curr.row + d[0];
-                int nc = curr.col + d[1];
-                if(isValid(nr,nc,n,m) && grid[nr][nc]==1){
+            for(int[] dis : dir){
+                int nr = dis[0] + curr.row;
+                int nc = dis[1] + curr.col;
+                if(isValid(nr, nc, n, m) && grid[nr][nc]==1){
                     q.offer(new Node(nr, nc, curr.time+1));
                     grid[nr][nc] = 2;
                     fresh--;
@@ -43,6 +45,13 @@ class Solution {
                 }
             }
         }
-        return (fresh==0) ? mintime : -1;
+
+        return fresh == 0 ? mintime : -1;
+    }
+    public boolean isValid(int nr, int nc, int n, int m){
+        if(nr<n && nc<m && nr>=0 && nc>=0){
+            return true;
+        }
+        return false;
     }
 }
