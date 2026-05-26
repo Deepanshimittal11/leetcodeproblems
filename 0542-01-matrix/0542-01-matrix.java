6b45d0fd3c79c73f1055ad33faaa39a01200f2d4
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2026-05-26 12:03:44 +0530

    Time: 189 ms (5.28%), Space: 49.1 MB (46.76%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,34 +1,46 @@
+class Node{
+    int row, col, time;
+    Node(int row, int col, int time){
+        this.row = row;
+        this.col = col;
+        this.time = time;
+    }
+}
 class Solution {
+    int[][] dir = {{0,-1},{0,1},{1,0},{-1,0}};
     public int[][] updateMatrix(int[][] mat) {
-        int row = mat.length;
-        int col = mat[0].length;
+        int n = mat.length;
+        int m = mat[0].length;
 
-        int[][] vis = new int[row][col];
-        Queue<int[]> q = new LinkedList<>();
+        Queue<Node> q = new LinkedList<>();
+        boolean[][] vis = new boolean[n][m];
+        int cnt1 = 0;
 
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(mat[i][j]==0){
-                    q.offer(new int[]{i,j});
-                }else{
-                    vis[i][j] = -1;
-                }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0) q.offer(new Node(i, j, 0));
+                else cnt1++;
             }
         }
 
-        int[][] dir = {{0,-1},{0,1},{1,0},{-1,0}};
+        if(cnt1 == 0) return mat;
 
         while(!q.isEmpty()){
-            int[] curr = q.poll();
-            int r = curr[0], c = curr[1];
+            Node curr = q.poll();
             for(int[] d : dir){
-                int nr = r + d[0];
-                int nc = c + d[1];
-                if(nr<0 || nc<0 || nr>=row || nc>=col || vis[nr][nc]!=-1) continue;
-                vis[nr][nc] = vis[r][c] + 1;
-                q.offer(new int[]{nr,nc});
-            }            
+                int nr = d[0] + curr.row;
+                int nc = d[1] + curr.col;
+                if(isValid(nr, nc, n, m) && mat[nr][nc]==1 && !vis[nr][nc]){
+                    q.offer(new Node(nr, nc, curr.time+1));
+                    vis[nr][nc] = true;
+                    mat[nr][nc] = curr.time + 1;
+                }
+            }
         }
-        return vis;
+        return mat;
+    }
+    public boolean isValid(int nr, int nc, int n, int m){
+        if(nr<n && nr>=0 && nc<m && nc>=0) return true;
+        return false;
     }
 }
