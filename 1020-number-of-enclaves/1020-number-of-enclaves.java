4dcf9247a746dbadd953c89db244fc7d13d4930d
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2026-05-26 15:37:29 +0530

    Time: 8 ms (71.17%), Space: 64.7 MB (9.97%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,41 +1,45 @@
 class Solution {
+    int[][] dir = {{0,-1},{0,1},{1,0},{-1,0}};
     public int numEnclaves(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        Queue<int[]> q = new LinkedList<>();
 
-        //add all boundary's 1's:
         for(int i=0;i<n;i++){
-            if(grid[i][0]==1) q.offer(new int[]{i,0});
-            if(grid[i][m-1]==1) q.offer(new int[]{i,m-1});
+            if(grid[i][0]==1){
+                dfs(grid, i, 0, n, m);
+            }
+            if(grid[i][m-1]==1){
+                dfs(grid, i, m-1, n, m);
+            }
         }
         for(int j=0;j<m;j++){
-            if(grid[0][j]==1) q.offer(new int[]{0,j});
-            if(grid[n-1][j]==1) q.offer(new int[]{n-1,j});
-        }
-
-        int[][] dir = {{0,-1},{0,1},{1,0},{-1,0}};
-
-        while(!q.isEmpty()){
-            int[] cell = q.poll();
-            int i = cell[0], j=cell[1];
-
-            if(i<0 || j<0 || i>=n || j>=m || grid[i][j]==0) continue;
-            grid[i][j]=0;
-
-            for(int[] d : dir){
-                int ni = i + d[0];
-                int nj = j + d[1];
-                q.offer(new int[]{ni,nj});
+            if(grid[0][j]==1){
+                dfs(grid, 0, j, n, m);
+            }
+            if(grid[n-1][j]==1){
+                dfs(grid, n-1, j, n, m);
             }
         }
 
         int cnt = 0;
-        for(int[] row : grid){
-            for(int val : row){
-                if(val==1) cnt++;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1) cnt++;
             }
         }
         return cnt;
+    }
+    public void dfs(int[][] grid, int i, int j, int n, int m){
+        grid[i][j] = 0;
+        for(int[] d : dir){
+            int nr = d[0] + i;
+            int nc = d[1] + j;
+            if(isValid(nr, nc, n, m) && grid[nr][nc]==1){
+                dfs(grid, nr, nc, n, m);
+            }
+        }
+    }
+    public boolean isValid(int nr, int nc, int n, int m){
+        return nr<n && nr>=0 && nc<m && nc>=0;
     }
 }
