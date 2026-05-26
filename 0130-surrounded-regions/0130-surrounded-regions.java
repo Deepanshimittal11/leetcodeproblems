fe4168e7c50bed626c1f088ab3376af59c982d12
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2026-05-26 12:31:13 +0530

    Time: 2 ms (75.91%), Space: 47.8 MB (37.45%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,38 +1,49 @@
 class Solution {
+    int[][] dir = {{0,-1},{0,1},{-1,0},{1,0}};
     public void solve(char[][] board) {
-        int row = board.length;
-        int col = board[0].length;
+        int n = board.length;
+        int m = board[0].length;
 
-        for(int i=0;i<row;i++){
-            dfs(board, i, 0);
-            dfs(board, i, col-1);
+        for(int i=0;i<n;i++){
+            if(board[i][0]=='O'){
+                dfs(board, i, 0, n, m);
+            }
+            if(board[i][m-1]=='O'){
+                dfs(board, i, m-1, n, m);
+            }
         }
-        for(int j=0;j<col;j++){
-            dfs(board, 0, j);
-            dfs(board, row-1, j);
+
+        for(int j=0;j<m;j++){
+            if(board[0][j]=='O'){
+                dfs(board, 0, j, n, m);
+            }
+            if(board[n-1][j]=='O'){
+                dfs(board, n-1, j, n, m);
+            }
         }
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){            
-                if(board[i][j]=='O'){
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j] == 'O'){
                     board[i][j] = 'X';
                 }
-                else if(board[i][j]=='T'){
+                else if(board[i][j] == '1'){
                     board[i][j] = 'O';
                 }
             }
         }
     }
-    public void dfs(char[][] board, int i, int j){
-        int row = board.length;
-        int col = board[0].length;
-
-        if(i<0 || j<0 || i>=row || j>=col || board[i][j]!='O') return;
-
-        board[i][j] = 'T';
-
-        dfs(board, i+1, j);
-        dfs(board, i-1, j);
-        dfs(board, i, j+1);
-        dfs(board, i, j-1);
+    public void dfs(char[][] board, int i, int j, int n, int m){
+        board[i][j] = '1';
+        for(int[] d : dir){
+            int nr = i + d[0];
+            int nc = j + d[1];
+            if(isValid(nr, nc, n, m) && board[nr][nc] == 'O'){
+                dfs(board, nr, nc, n, m);
+            }
+        }
+    }
+    public boolean isValid(int nr, int nc, int n, int m){
+        return nr<n && nr>=0 && nc<m && nc>=0;
     }
 }
