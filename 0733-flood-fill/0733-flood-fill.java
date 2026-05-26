7cf564fa7076c3ad6e6d4c0835ec0a4357a98dc2
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2026-05-26 10:56:44 +0530

    Time: 1 ms (36.82%), Space: 47.1 MB (10.47%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,31 +1,34 @@
 class Solution {
+    int[][] dir = {{0,-1},{0,1},{1,0},{-1,0}};
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int row = image.length;
-        int col = image[0].length;
+        int original_color = image[sr][sc];
 
-        int originalcolor = image[sr][sc];
-        if(originalcolor == color) return image;
-        
-        int[][] dir = {{0,1},{0,-1},{-1,0},{1,0}};
+        boolean[][] vis = new boolean[image.length][image[0].length];
 
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{sr,sc});
-        image[sr][sc] = color;
+        dfs(image, sr, sc, original_color, color, vis);
 
-        while(!q.isEmpty()){
-            int size = q.size();
-            for(int i=0;i<size;i++){
-                int[] curr = q.poll();
-                int r = curr[0], c = curr[1];
-                for(int[] d : dir){
-                    int nr = r+d[0];
-                    int nc = c+d[1];
-                    if(nr<0 || nc<0 || nr>=row || nc>=col || image[nr][nc] != originalcolor) continue;
-                    image[nr][nc] = color;
-                    q.offer(new int[]{nr,nc});
-                }
-            }
-        }
         return image;
+    }
+    public void dfs(int[][] image, int sr, int sc, int original_color, int color, boolean[][] vis){
+        int n = image.length;
+        int m = image[0].length;
+
+        image[sr][sc] = color;
+        vis[sr][sc] = true;
+
+        for(int[] d : dir){
+            int nr = sr + d[0];
+            int nc = sc + d[1];
+            if(isValid(nr, nc, n, m) && image[nr][nc]==original_color && !vis[nr][nc]){
+                dfs(image, nr, nc, original_color, color, vis);
+                image[nr][nc] = color;
+            } 
+        }
+    }
+    public boolean isValid(int nr, int nc, int n , int m){
+        if(nr<n && nc<m && nr>=0 && nc>=0){
+            return true;
+        }
+        return false;
     }
 }
