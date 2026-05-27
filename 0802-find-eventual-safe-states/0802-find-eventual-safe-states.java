aeca10284590d4d27ba626bd275e4261105a254a
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2026-05-27 17:30:25 +0530

    Time: 5 ms (69.95%), Space: 65.5 MB (9.17%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,38 +1,37 @@
 class Solution {
-    public boolean isCycle(int[][] graph, int i, boolean[] vis, boolean[] isRecursion){
-        vis[i] = true;
-        isRecursion[i] = true;
-
-        for(int v : graph[i]){
-            if(vis[v]==false && isCycle(graph, v, vis, isRecursion)){
-                return true;
-            }
-            else if(isRecursion[v]==true){
-                return false;
-            }
-        }
-
-        isRecursion[i] = false;
-        return false;
-    }
     public List<Integer> eventualSafeNodes(int[][] graph) {
         int V = graph.length;
-        boolean[] vis = new boolean[V];
-        boolean[] isRecursion = new boolean[V];
+        int[] vis = new int[V];
+        int[] pathvis = new int[V];
+        int[] check = new int[V];
 
         for(int i=0;i<V;i++){
-            if(!vis[i]){
-                isCycle(graph, i, vis, isRecursion);
+            if(vis[i]==0){
+                dfs(graph, vis, pathvis, check, i, V);
             }
         }
 
-        List<Integer> ans = new ArrayList<>();
-
+        List<Integer> safenodes = new ArrayList<>();
         for(int i=0;i<V;i++){
-            if(isRecursion[i] == false){
-                ans.add(i);
+            if(check[i]==1) {
+                safenodes.add(i);
             }
         }
-        return ans;
+        return safenodes;
+    }
+    public boolean dfs(int[][] graph, int[] vis, int[] pathvis, int[] check, int i, int V){
+        vis[i] = 1;
+        pathvis[i] = 1;
+
+        for(int newnode : graph[i]){
+            if(vis[newnode]==0){
+                if(dfs(graph, vis, pathvis, check, newnode, V) == true) return true;
+            }
+            else if(pathvis[newnode] == 1) return true;
+        }
+        
+        check[i] = 1;
+        pathvis[i] = 0;
+        return false;
     }
 }
