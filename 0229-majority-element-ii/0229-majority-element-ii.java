9189df59100a90f5d6e47c0f5fbfe59f8545b019
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2026-06-16 11:42:18 +0530

    Time: 3 ms (73.08%), Space: 52.5 MB (51.94%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,39 +1,42 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> result = new ArrayList<>();
-        
-        if (nums == null || nums.length == 0) {
-            return result; 
-        }
+        List<Integer> ans = new ArrayList<>();
+        int n = nums.length;
+        //we'll take elem1 and elem2 as most minimum value b/c nums[i] can be zero.
+        int cnt1 = 0, elem1 = Integer.MIN_VALUE;
+        int cnt2 = 0, elem2 = Integer.MIN_VALUE;
 
-        int candidate1 = Integer.MIN_VALUE, candidate2 = Integer.MIN_VALUE;
-        int count1 = 0, count2 = 0;
-
-        for (int num : nums) {
-            if (num == candidate1) {
-                count1++;
-            } else if (num == candidate2) {
-                count2++;
-            } else if (count1 == 0) {
-                candidate1 = num;
-                count1 = 1;
-            } else if (count2 == 0) {
-                candidate2 = num;
-                count2 = 1;
-            } else {
-                count1--;
-                count2--;
+        for(int i=0;i<n;i++){
+            if(cnt1==0 && nums[i]!=elem2){
+                cnt1 = 1;
+                elem1 = nums[i];
+            }
+            else if(cnt2==0 && nums[i]!=elem1){
+                cnt2 = 1;
+                elem2 = nums[i];
+            }
+            else if(nums[i] == elem1){
+                cnt1++;
+            }
+            else if(nums[i] == elem2){
+                cnt2++;
+            }
+            else{
+                cnt1--;
+                cnt2--;
             }
         }
-        count1 = count2 = 0;
-        for (int num : nums) {
-            if (num == candidate1) count1++;
-            if (num == candidate2) count2++;
+
+        //now we don't need cnt1 and cnt2, we just need them to know our elem1 and elem2.
+        //so we'll make cnt1 and cnt2 = 0;
+        cnt1 = 0;
+        cnt2 = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i] == elem1) cnt1++;
+            if(nums[i] == elem2) cnt2++;
         }
-
-        if (count1 > nums.length / 3) result.add(candidate1);
-        if (count2 > nums.length / 3) result.add(candidate2);
-
-        return result;
+        if(cnt1 > n/3) ans.add(elem1);
+        if(cnt2 > n/3) ans.add(elem2);
+        return ans;
     }
 }
