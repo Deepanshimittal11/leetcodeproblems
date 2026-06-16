309caf01665c8770071c8a529a19b64f3625fe72
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2026-06-16 13:35:25 +0530

    Time: 0 ms (100%), Space: 42.2 MB (27.99%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,28 +1,19 @@
 class Solution {
-    //recursion:
-    // public int uniquePaths(int m, int n) {
-    //     return helper(0,0,m,n);
-    // }
-    // public int helper(int i, int j, int m, int n){
-    //     if(i==m-1 && j==n-1) return 1;
-    //     if(i>=m || j>=n) return 0;
-    //     int right = helper(i, j+1, m, n);
-    //     int bottom = helper(i+1, j, m, n);
-    //     return right + bottom;
-    // }
-
-    //dp:
+    int[][] dp;
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m+1][n+1];
-        for(int[] rows : dp) Arrays.fill(rows, -1);
-        return helper(0,0,m,n,dp);
+        // down or right only.
+        dp = new int[m][n];
+        for(int[] row : dp){
+            Arrays.fill(row, -1);
+        }
+        return helper(0, 0, m, n, 0, dp);
     }
-    public int helper(int i, int j, int m, int n, int[][] dp){
-        if(i==m-1 && j==n-1) return 1;
+    public int helper(int i, int j, int m, int n, int ans, int[][] dp){
         if(i>=m || j>=n) return 0;
+        if(i==m-1 && j==n-1) return 1;
         if(dp[i][j] != -1) return dp[i][j];
-        int down = helper(i+1, j, m, n, dp);
-        int right = helper(i, j+1, m, n, dp);
-        return dp[i][j] = down + right;
+        
+        dp[i][j] = helper(i+1, j, m, n, ans, dp) + helper(i, j+1, m, n, ans, dp);
+        return dp[i][j];
     }
 }
