f2cdcb17f2105fc0a77bc0a6b6e0302e9bc03d34
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2026-06-30 01:24:07 +0530

    Time: 482 ms (34.83%), Space: 0B (100%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -1,12 +1,11 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-    set n = n-1;
+set n = n-1;
   RETURN (
       # Write your MySQL query statement below.
-    --   distinct is used nth (dynamic) elements.
-        select distinct salary 
+        select distinct salary as getNthHighestSalary
         from Employee
         order by salary desc
-        LIMIT 1 offset n       
+        limit 1 offset n
   );
 END
