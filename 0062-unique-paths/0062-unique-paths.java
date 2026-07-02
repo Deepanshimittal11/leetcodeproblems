3e59778ec1bf8456f30e98119bcb0d91bc4e3501
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2026-07-02 23:14:33 +0530

    Time: 0 ms (100%), Space: 42.1 MB (47.43%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,19 +1,19 @@
 class Solution {
-    int[][] dp;
     public int uniquePaths(int m, int n) {
-        // down or right only.
-        dp = new int[m][n];
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
+        int[][] dp = new int[m][n];
+        for(int[] rows : dp){
+            Arrays.fill(rows, -1);
         }
-        return helper(0, 0, m, n, 0, dp);
+        return helper(m-1, n-1, dp);
     }
-    public int helper(int i, int j, int m, int n, int ans, int[][] dp){
-        if(i>=m || j>=n) return 0;
-        if(i==m-1 && j==n-1) return 1;
+    public int helper(int i, int j, int[][] dp){
+        if(i==0 && j==0) return 1;
+        if(i<0 || j<0) return 0;
         if(dp[i][j] != -1) return dp[i][j];
-        
-        dp[i][j] = helper(i+1, j, m, n, ans, dp) + helper(i, j+1, m, n, ans, dp);
-        return dp[i][j];
+
+        int left = helper(i, j-1, dp);
+        int up = helper(i-1, j, dp);
+
+        return dp[i][j] = left+up;
     }
 }
