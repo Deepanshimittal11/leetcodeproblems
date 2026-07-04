02a1ed543de2d9baf425995bba4da4ddc43968f2
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2026-07-04 23:52:28 +0530

    Time: 18 ms (24.24%), Space: 58.2 MB (6.5%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,20 +1,24 @@
 class Solution {
     public int change(int amount, int[] coins) {
         int n = coins.length;
-        int[][] t = new int[n+1][amount+1];
+        int[][] dp = new int[amount+1][n+1];
+        for(int[] rows : dp){
+            Arrays.fill(rows, -1);
+        }
+        return helper(amount, coins, n-1, dp);
+    }
+    public int helper(int amt, int[] coins, int ind, int[][] dp){
+        if(ind==0){
+            if(amt%coins[0] == 0) return 1;
+            return 0;
+        }
+        if(dp[amt][ind] != -1) return dp[amt][ind];
 
-        for(int i=0;i<=n;i++){
-            t[i][0] = 1; 
+        int notTake = helper(amt, coins, ind-1, dp);
+        int take = 0;
+        if(coins[ind] <= amt){
+            take = helper(amt-coins[ind], coins, ind, dp);
         }
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=amount;j++){
-                if(coins[i-1]<=j){
-                    t[i][j] = (t[i][j-coins[i-1]] + t[i-1][j]);
-                }else{
-                    t[i][j] = t[i-1][j];
-                }
-            }
-        }
-        return t[n][amount];
+        return dp[amt][ind] = take+notTake;
     }
 }
