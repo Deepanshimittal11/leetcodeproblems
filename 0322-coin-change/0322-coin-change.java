2530effc566e3b3c5cd745dd88f136cd959cc0df
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2026-07-04 23:38:27 +0530

    Time: 30 ms (19.04%), Space: 47 MB (28.77%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,17 +1,28 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int inf = (int)1e9;
-
-        int[] dp = new int[amount+1];
-
-        Arrays.fill(dp, inf);
-        dp[0] = 0;
-        
-        for(int coin : coins){
-            for(int i=coin;i<=amount;i++){
-                dp[i] = Math.min(dp[i], dp[i-coin]+1);
-            }
+        int n = coins.length;
+        int[][] dp = new int[n+1][amount+1];
+        for(int[] rows : dp){
+            Arrays.fill(rows, -1);
         }
-        return dp[amount] >= inf ? -1 : dp[amount];
+
+        int ans = helper(coins, amount, n-1, dp);
+        if(ans >= (int)1e9) return -1;
+        return ans;
+    }
+    public int helper(int[] coins, int amount, int ind, int[][] dp){
+        if(ind == 0){
+            if(amount % coins[0] == 0) return amount/coins[0];
+            return Integer.MAX_VALUE;
+        }
+        if(dp[ind][amount] != -1) return dp[ind][amount];
+
+        int notTake = helper(coins, amount, ind-1, dp);
+        int take = (int)1e9;
+        if(coins[ind] <= amount){
+            take = 1 + helper(coins, amount-coins[ind], ind, dp);
+        }
+
+        return dp[ind][amount] = Math.min(take, notTake);
     }
 }
