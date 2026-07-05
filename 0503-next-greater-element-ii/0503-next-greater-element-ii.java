e1fe56cbfa5ec9c056a88e17aed0e79abf7598cc
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2026-07-05 10:46:09 +0530

    Time: 20 ms (13.98%), Space: 48.3 MB (75.31%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,39 +1,27 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int n = nums.length;
-        int[] list = new int[2*n];
-        Stack<Integer> st = new Stack<>();
-
+        int[] circularArr = new int[2*n];
         for(int i=0;i<n;i++){
-            list[i] = nums[i];
-            list[i+n] = nums[i];
+            circularArr[i] = nums[i];
+            circularArr[n+i] = nums[i];
         }
 
-        //nums = [1,2,1];
-        //list = [1,2,1,1,2,1];
-
-        List<Integer> temp = new ArrayList<>();
-        int m = list.length;
-
-        for(int i=m-1;i>=0;i--){
-            while(!st.isEmpty() && st.peek()<=list[i]){
+        int[] ans = new int[2*n];
+        Stack<Integer> st = new Stack<>();
+        for(int i=ans.length-1;i>=0;i--){
+            while(!st.isEmpty() && st.peek()<=circularArr[i]){
                 st.pop();
             }
-
             if(st.isEmpty()){
-                temp.add(-1);
+                ans[i] = -1;
             }
-            else{
-                temp.add(st.peek());
+            else if(st.peek() > circularArr[i]){
+                ans[i] = st.peek();
             }
-            st.push(list[i]);
+            st.push(circularArr[i]);
         }
-
-        int[] ans = new int[n];
-        Collections.reverse(temp);
-        for(int i=0;i<n;i++){
-            ans[i] = temp.get(i);
-        }
+        ans = Arrays.copyOfRange(ans, 0, n);
         return ans;
     }
 }
