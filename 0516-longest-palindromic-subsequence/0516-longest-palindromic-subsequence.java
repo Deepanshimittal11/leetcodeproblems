35c1c6e9e00991eff57e573dd13b4f9f24e98873
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2026-07-07 16:58:56 +0530

    Time: 46 ms (48.71%), Space: 63.9 MB (45.32%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,26 +1,32 @@
 class Solution {
-    public int longestPalindromeSubseq(String s) {
-        String reverse = new StringBuilder(s).reverse().toString();
-        int n = s.length();
-        int m = reverse.length();
+    public int longestPalindromeSubseq(String s1) {
+        int n = s1.length()-1;
+        StringBuilder str = new StringBuilder();
+        for(int i=n;i>=0;i--){
+            str.append(s1.charAt(i));
+        }
+        String s2 = str.toString();
 
-        int[][] t = new int[n+1][m+1];
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=m;j++){
-                if(i==0 || j==0){
-                    t[i][j] = 0;
-                }
-            }
+        int m =s2.length()-1;
+        int[][] dp = new int[n+1][m+1];
+        for(int[] rows : dp){
+            Arrays.fill(rows, -1);
         }
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(s.charAt(i-1)==reverse.charAt(j-1)){
-                    t[i][j] = 1 + t[i-1][j-1];
-                }else{
-                    t[i][j] = Math.max(t[i-1][j], t[i][j-1]);
-                }
-            }
+
+        return helper(s1, s2, n, m, dp);
+    }
+    public int helper(String s1, String s2, int i, int j, int[][] dp){
+        int take = 0;
+        int notTake = 0;
+        if(i<0 || j<0) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        if(s1.charAt(i) == s2.charAt(j)){
+            take = 1 + helper(s1, s2, i-1, j-1, dp);
         }
-        return t[n][m];
+        else{
+            notTake = Math.max(helper(s1, s2, i-1, j, dp), helper(s1, s2, i, j-1, dp));
+        }
+        return dp[i][j] = (take + notTake);
     }
 }
