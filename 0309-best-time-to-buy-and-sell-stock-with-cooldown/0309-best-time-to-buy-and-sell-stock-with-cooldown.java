10ef354b10e9111b8aad685a5842d2c8594ecfea
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2026-07-13 18:40:02 +0530

    Time: 1 ms (70.63%), Space: 43.2 MB (89.31%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,18 +1,26 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        return helper(prices, 0, 1);
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        for(int[] rows : dp){
+            Arrays.fill(rows, -1);
+        }
+        return helper(prices, n, 0, 1, dp);
     }
-    public int helper(int[] arr, int i, int canbuy){
-        if(i >= arr.length) return 0;
+    public int helper(int[] prices, int n, int ind, int canbuy, int[][] dp){
+        int profit = 0;
+        if(ind>=n) return 0;
+        if(dp[ind][canbuy] != -1) return dp[ind][canbuy];
+
         if(canbuy == 1){
-            int buy = -arr[i] + helper(arr, i+1, 0);
-            int skip = helper(arr, i+1, 1);
-            return Math.max(skip, buy);
+            int buy = -prices[ind] + helper(prices, n, ind+1, 0, dp);
+            int skip = helper(prices, n, ind+1, 1, dp);
+            profit += Math.max(buy, skip);
+        }else{ 
+            int sell = prices[ind] + helper(prices, n, ind+2, 1, dp);
+            int hold = helper(prices, n, ind+1, 0, dp);
+            profit += Math.max(sell, hold);
         }
-        else{
-            int sell = arr[i] + helper(arr, i+2, 1);
-            int hold = helper(arr, i+1, 0);
-            return Math.max(hold, sell);
-        }
+        return dp[ind][canbuy] = profit;
     }
 }
