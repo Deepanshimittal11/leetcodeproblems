ebc4e723a724e28bf8fe480344704eba363b2682
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2026-07-19 18:29:44 +0530

    Time: 255 ms (97.12%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,7 +1,11 @@
 # Write your MySQL query statement below
+
+#agr inner quesry se koi value nhi aaegi to outer query usse null return kr degi
+#maan lo sirf ek hi salary given to us testcase me hme yeh use krna pdega
+
 select(
-    select distinct salary 
+    select distinct salary
     from Employee
     order by salary desc
     limit 1 offset 1
-) as SecondHighestSalary;
+) as SecondHighestSalary
