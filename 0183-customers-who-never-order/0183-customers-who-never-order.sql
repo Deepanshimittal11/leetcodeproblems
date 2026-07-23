67c1d1c837c8e8cd6ed1f2b87e726ae1735631a4
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2026-07-23 11:06:23 +0530

    Time: 568 ms (62.3%), Space: 0B (100%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -1,11 +1,6 @@
 # Write your MySQL query statement below
-
-#we have to find the customer id which is not present in customerId of orders list.
-#vo customer ki jo orders ki customerid list me nhi h
-
 select c.name as Customers
 from Customers c
-where c.id not in (
-    select o.customerId
-    from Orders o
-)
+left join Orders o
+on c.id = o.customerId
+where o.id is null
