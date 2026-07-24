8361992c3e391cfcf19bdccd84ccb76e4212a830
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2026-07-24 13:18:06 +0530

    Time: 267 ms (86.82%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -3,9 +3,15 @@
 #agr inner quesry se koi value nhi aaegi to outer query usse null return kr degi
 #maan lo sirf ek hi salary given to us testcase me hme yeh use krna pdega
 
-select(
-    select distinct salary
-    from Employee
-    order by salary desc
-    limit 1 offset 1
-) as SecondHighestSalary
+#select(
+ #   select distinct salary
+  #  from Employee
+   # order by salary desc
+    #limit 1 offset 1
+#) as SecondHighestSalary
+
+select max(salary) as SecondHighestSalary
+from Employee
+where salary < (
+    select max(salary) from Employee
+) 
