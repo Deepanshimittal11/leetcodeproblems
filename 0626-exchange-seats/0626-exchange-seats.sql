281d2be9505c6ac05b6e6a811053a79d2e0f9e41
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2026-07-24 13:03:59 +0530

    Time: 810 ms (5.02%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,10 +1,9 @@
 # Write your MySQL query statement below
-select 
+select
 case
-    when id%2=1 and id < (select count(*) from seat) then id+1
-    when id%2=0 then id-1
-    else id
-    end as id,
-    student 
-from seat
-order by id;
+when id%2=1 && id<(select count(*) from Seat) then id+1
+when id%2=0 then id-1
+else id
+end as id, student
+from Seat
+order by id
