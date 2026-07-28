42d08d6621fbab578afde9fa139ac2c6fa963ee2
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2026-07-28 19:43:03 +0530

    Time: 3 ms (98.24%), Space: 47 MB (71.75%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -2,19 +2,26 @@ class Solution {
     public int minDistance(String word1, String word2) {
         int n = word1.length();
         int m = word2.length();
-        int[][] dp = new int[n][m];
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
+        int[][] dp = new int[n+1][m+1];
+        for(int[] rows : dp){
+            Arrays.fill(rows, -1);
         }
-        return helper(n-1, word1, m-1, word2, dp);
+        return helper(word1, word2, n-1, m-1, dp);
     }
-    public int helper(int i, String word1, int j, String word2, int[][] dp){
+    public int helper(String w1, String w2, int i, int j, int[][] dp){
+        int take=0;
+        int nottake=0;
+
         if(i<0) return j+1;
         if(j<0) return i+1;
         if(dp[i][j] != -1) return dp[i][j];
 
-        if(word1.charAt(i)==word2.charAt(j)) return dp[i][j] = helper(i-1, word1, j-1, word2, dp);
-
-        return dp[i][j] = 1 + Math.min(helper(i-1, word1, j, word2, dp), Math.min(helper(i, word1, j-1, word2, dp), helper(i-1, word1, j-1, word2, dp)));
+        if(w1.charAt(i)==w2.charAt(j)){
+            take = helper(w1,w2,i-1,j-1, dp);
+        }
+        else{
+            nottake = 1 + Math.min(helper(w1,w2,i-1,j-1, dp), Math.min(helper(w1,w2,i-1,j, dp), helper(w1,w2,i,j-1, dp)));
+        }
+        return dp[i][j] = take+nottake;
     }
 }
